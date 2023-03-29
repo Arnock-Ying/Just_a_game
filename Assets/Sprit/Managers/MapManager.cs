930a9f8014d15bc0bbb216fd725d9fcadf4da922
L-Bromine <lishaofan_31415926@163.com>
2023-03-29 23:41:17 +0800

    添加了实现，但是没有调试

--- Assets/Sprit/Managers/MapManager.cs
+++ Assets/Sprit/Managers/MapManager.cs
@@ -15,11 +15,15 @@ namespace Manager
 		public static Dictionary<Vector2Int, BlockControl> Blocks { get { return instend.blockList; } }
 		public static Block GetBlock(int x, int y)
 		{
-			return instend.blockList[new Vector2Int(Mathf.FloorToInt(x / 256), Mathf.FloorToInt(y / 256))].GetBlock(x % 256, y % 256);
+			if (instend.blockList.ContainsKey(new Vector2Int(Mathf.FloorToInt(x / 256), Mathf.FloorToInt(y / 256))))
+				return instend.blockList[new Vector2Int(Mathf.FloorToInt(x / 256), Mathf.FloorToInt(y / 256))].GetBlock(x % 256, y % 256);
+			else return null;
 		}
 
-		public static void GetBlock(int x, int y, Block block)
+		public static void SetBlock(int x, int y, Block block)
 		{
+			if (!instend.blockList.ContainsKey(new Vector2Int(Mathf.FloorToInt(x / 256), Mathf.FloorToInt(y / 256))))
+				instend.blockList[new Vector2Int(Mathf.FloorToInt(x / 256), Mathf.FloorToInt(y / 256))] = new();
 			instend.blockList[new Vector2Int(Mathf.FloorToInt(x / 256), Mathf.FloorToInt(y / 256))].SetBlock(x % 256, y % 256, block);
 		}
 		public void Awake()
